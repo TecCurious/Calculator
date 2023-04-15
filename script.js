dd23d746fa2d8fc9386fedda7f18b95c11175d89
TecCurious <120739210+TecCurious@users.noreply.github.com>
2023-04-15 16:57:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button)=>{
+
+button.addEventListener('click',(e)=>{
+    if(e.target.innerHTML == "="){
+        string = eval(string);
+        document.querySelector('input').value =string;
+        
+    }
+   else if(e.target.innerHTML == "Ac"){
+        string = "";
+        document.querySelector('input').value =string;
+    }
+   else if(e.target.innerHTML == "Del"){
+       string = string.substr(-99, string.length-1);
+        document.querySelector('input').value =string;
+    }
+
+    else{
+    console.log(e.target);
+    string = string + e.target.innerHTML;
+  document.querySelector('input').value =string;
+    }
+})
+
+})
